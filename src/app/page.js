b2e7fdaa54474c3c7ff710fcf5044bed520dbdd2
Gareth Flynn <gareth.t.flynn@gmail.com>
2024-04-12 09:49:52 -0400

    serverActions added for todos

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,19 @@ function getTodos() {
   return prisma.todo.findMany();
 }
 
+async function toggleTodo(id, complete) {
+  "use server"
+
+  // console.log(id, complete)
+  await prisma.todo.update({ where: { id }, data: { complete } })
+}
+
 export default async function Home() {
   const todos = await getTodos();
   return (
     <>
       <header className="flex justify-between items-center mb-4">
-        <h1 className="p-4 text-2xl container mx-auto">Todos</h1>
+        <h1 className="p-4 text-4xl container mx-auto">Todos</h1>
         <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus:within:bg-slate-700 outline-none"
           href="/new"
@@ -20,8 +27,8 @@ export default async function Home() {
         </Link>
       </header>
       <ul className="pl-4">
-        {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo}/>
+        {todos.map(todo => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
